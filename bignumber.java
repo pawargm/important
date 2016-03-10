6b9a7a905df6845228114b3a978726bf45c10374
pawargm <gopalpawar299@gmail.com>
2016-03-10 21:17:13 +0530

    Create bignumber.java

---
+++ bignumber.java
@@ -0,0 +1,12 @@
+//BIGNUMBER class in java
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        BigInteger a = new BigInteger(in.next());
+        BigInteger b = new BigInteger(in.next());
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
